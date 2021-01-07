d106d6379a627ad367b666710863fce3c8009c4c
Justin Klemm <jwklemm@gmail.com>
2021-01-06 23:53:03 -0800

    Add Prettier and apply

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,21 @@ module.exports = (grunt) => {
   grunt.initConfig({
     ghostinspector: {
       options: {
-        apiKey: process.env.GHOST_INSPECTOR_API_KEY
+        apiKey: process.env.GHOST_INSPECTOR_API_KEY,
       },
       test1: {
-        suites: ['53cf58c0350c6c41029a11be']
+        suites: ['53cf58c0350c6c41029a11be'],
       },
       test2: {
-        tests: ['53cf58fc350c6c41029a11bf', '53cf59e0350c6c41029a11c0']
+        tests: ['53cf58fc350c6c41029a11bf', '53cf59e0350c6c41029a11c0'],
       },
       test3: {
         tests: ['53cf58fc350c6c41029a11bf'],
         options: {
-          startUrl: 'https://www.google.com.br'
-        }
-      }
-    }
+          startUrl: 'https://www.google.com.br',
+        },
+      },
+    },
   })
   // Load grunt modules
   grunt.loadTasks('tasks')
--- tasks/ghostinspector.js
+++ tasks/ghostinspector.js
@@ -27,53 +27,61 @@ module.exports = (grunt) => {
     if (suites.length) {
       grunt.log.writeln('Executing suites...')
     }
-    async.eachSeries(suites, (suiteId, done) => {
-      // execute suite
-      GhostInspector.executeSuite(suiteId, options, (err, data, passing) => {
-        // evaluate api response
-        if (err) {
-          return done(`Error executing suite "${suiteId}": ${err}`)
-        }
-        if (passing) {
-          grunt.log.ok(`Suite "${suiteId}" passed.`)
-          return done()
-        } else {
-          return done(`Suite "${suiteId}" failed.`)
-        }
-      })
-    }, (err) => {
-      // done with suites, bail if we hit an error/failure
-      if (err) {
-        grunt.log.error(err)
-        return gruntDone(false)
-      }
-      // execute any specified tests
-      if (tests.length) {
-        grunt.log.writeln('Executing tests...')
-      }
-      async.eachSeries(tests, (testId, done) => {
-        // execute test
-        GhostInspector.executeTest(testId, options, (err, data, passing) => {
+    async.eachSeries(
+      suites,
+      (suiteId, done) => {
+        // execute suite
+        GhostInspector.executeSuite(suiteId, options, (err, data, passing) => {
           // evaluate api response
           if (err) {
-            return done(`Error executing test "${data.test.name}" (${testId}): ${err}`)
+            return done(`Error executing suite "${suiteId}": ${err}`)
           }
           if (passing) {
-            grunt.log.ok(`Test "${data.test.name}" (${testId}) passed.`)
+            grunt.log.ok(`Suite "${suiteId}" passed.`)
             return done()
           } else {
-            return done(`Test "${data.test.name}" (${testId}) failed`)
+            return done(`Suite "${suiteId}" failed.`)
           }
         })
-      }, (err) => {
-        // done with tests, bail if we hit an error/failure
+      },
+      (err) => {
+        // done with suites, bail if we hit an error/failure
         if (err) {
           grunt.log.error(err)
           return gruntDone(false)
         }
-        // done with suites and tests
-        return gruntDone()
-      })
-    })
+        // execute any specified tests
+        if (tests.length) {
+          grunt.log.writeln('Executing tests...')
+        }
+        async.eachSeries(
+          tests,
+          (testId, done) => {
+            // execute test
+            GhostInspector.executeTest(testId, options, (err, data, passing) => {
+              // evaluate api response
+              if (err) {
+                return done(`Error executing test "${data.test.name}" (${testId}): ${err}`)
+              }
+              if (passing) {
+                grunt.log.ok(`Test "${data.test.name}" (${testId}) passed.`)
+                return done()
+              } else {
+                return done(`Test "${data.test.name}" (${testId}) failed`)
+              }
+            })
+          },
+          (err) => {
+            // done with tests, bail if we hit an error/failure
+            if (err) {
+              grunt.log.error(err)
+              return gruntDone(false)
+            }
+            // done with suites and tests
+            return gruntDone()
+          },
+        )
+      },
+    )
   })
 }
